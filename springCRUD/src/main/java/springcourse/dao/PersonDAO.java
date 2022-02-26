acd345c7b8562c4aeb091476444b42729f351808
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-02-26 09:47:47 +0300

    add PATCH, DELETE

--- springCRUD/src/main/java/springcourse/dao/PersonDAO.java
+++ springCRUD/src/main/java/springcourse/dao/PersonDAO.java
@@ -6,6 +6,9 @@ import springcourse.models.Person;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Neil Alishev
+ */
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
@@ -31,5 +34,15 @@ public class PersonDAO {
     public void save(Person person) {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
     }
 }
